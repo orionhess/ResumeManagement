19ace6af2ab327189eac4aa8263558ad3f21b809
Orion Hess <orionhess@gmail.com>
2026-03-27 10:32:48 -0700

    feat(testing): set up uv and pytest with example tests Set up uv as a package manager and version management Set up pytest as a testing framework -> small examples included Create a makefile for easy running of tests and the program itself

---
+++ tests/test_tests.py
@@ -0,0 +1,19 @@
+# Are tests working?
+def test_tests_1():
+    assert 1 == 1
+
+
+def test_tests_2():
+    assert 2 * 2 == 4
+
+
+def test_tests_3():
+    assert 1 != 0
+
+
+def test_tests_4():
+    assert True == (True or False)
+
+
+def test_tests_5():
+    assert 2 <= 5
