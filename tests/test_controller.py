5c2c272a9f6eaf48d0d48816009a42ed3a9ca8d6
Orion Hess <orionhess@gmail.com>
2026-04-26 21:16:27 -0700

    feat: further test controller class

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,44 +1,46 @@
 from resumemanagement.controller import Controller
 
-items = [
-    {
-        "text": "section 1",
-        "selected": True,
-        "items": [
-            {"text": "item 1", "selected": True},
-            {"text": "item 2", "selected": False},
-            {"text": "item 3", "selected": True},
-        ],
-    },
-    {
-        "text": "section 2",
-        "selected": False,
-        "items": [
-            {"text": "item 4", "selected": False},
-            {"text": "item 5", "selected": False},
-            {"text": "item 6", "selected": False},
-        ],
-    },
-    {
-        "text": "section 3",
-        "selected": True,
-        "items": [
-            {"text": "item 7", "selected": True},
-            {"text": "item 8", "selected": False},
-            {"text": "item 9", "selected": False},
-        ],
-    },
-]
+
+def get_items():
+    return [
+        {
+            "text": "section 1",
+            "selected": True,
+            "items": [
+                {"text": "item 1", "selected": True},
+                {"text": "item 2", "selected": False},
+                {"text": "item 3", "selected": True},
+            ],
+        },
+        {
+            "text": "section 2",
+            "selected": False,
+            "items": [
+                {"text": "item 4", "selected": False},
+                {"text": "item 5", "selected": False},
+                {"text": "item 6", "selected": False},
+            ],
+        },
+        {
+            "text": "section 3",
+            "selected": True,
+            "items": [
+                {"text": "item 7", "selected": True},
+                {"text": "item 8", "selected": False},
+                {"text": "item 9", "selected": False},
+            ],
+        },
+    ]
 
 
 def test_get_sections():
-    controller: Controller = Controller(items)
+    controller: Controller = Controller(get_items())
     sections = controller.get_sections()
-    assert sections == items
+    assert sections == get_items()
 
 
 def test_get_items():
-    controller: Controller = Controller(items)
+    controller: Controller = Controller(get_items())
     gotten_items = controller.get_items("section 1")
     assert gotten_items == [
         {"text": "item 1", "selected": True},
@@ -48,7 +50,7 @@ def test_get_items():
 
 
 def test_toggle_section_item_status():
-    controller: Controller = Controller(items)
+    controller: Controller = Controller(get_items())
     controller.toggle_section_item_status("section 1", "item 2")
     gotten_items = controller.get_items("section 1")
     assert gotten_items == [
@@ -59,7 +61,7 @@ def test_toggle_section_item_status():
 
 
 def test_toggle_section_item_status_2():
-    controller: Controller = Controller(items)
+    controller: Controller = Controller(get_items())
     controller.toggle_section_item_status("section 3", "item 7")
     gotten_items = controller.get_items("section 3")
     assert gotten_items == [
@@ -69,3 +71,11 @@ def test_toggle_section_item_status_2():
     ]
     sections = controller.get_sections()
     assert not sections[2]["selected"]
+
+
+def test_get_sections_active():
+    controller: Controller = Controller(get_items())
+    active_sections = controller.get_sections_active()
+    assert len(active_sections) == 2
+    assert active_sections[0]["text"] == "section 1"
+    assert active_sections[1]["text"] == "section 3"
